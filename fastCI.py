2526b8b6fb0d50449f479b89eaf0c5f8a47b10e2
Stefan Heimersheim <heimersheim@ast.cam.ac.uk>
2022-07-19 15:19:10 +0100

    Fully-featured function

--- fastCI.py
+++ fastCI.py
@@ -2,8 +2,39 @@ import numpy as np
 import scipy.interpolate as sip
 import scipy.optimize as sop
 
-def credibility_interval(samples, weights=None, level=0.68):
-    assert level<1, "Level >= 1!"
+def credibility_interval(samples, weights=None, level=0.68, method="hpd"):
+    """Compute the credibility interval of weighted samples. Based on
+    linear interpolation of the cumulative density function, thus expected
+    discretization errors on the scale of distances between samples.
+    Parameters
+    ----------
+    samples: array
+        Samples to compute the credibility interval of.
+    samples: array, optional
+        Weights corresponding to samples. Default: ones
+    level: float, optional
+        Credibility level (probability, <1). Default: 0.68
+    method: str, optional
+        Which definition of interval to use. Default: 'hpd'
+        * hpd: Calculate highest posterior density (HPD) interval,
+          equivalent to iso-pdf interval (interval with same probability
+          density at each end) if distribution is unimodal.
+        * ll, ul: Lower limit, upper limit. One-sided limits for which
+          `level` fraction of the samples lie above / below the limit.
+        * et: Equal-tailed interval with the same fraction of samples
+          below and above the interval region.
+    Returns
+    -------
+    limit: tuple or float
+        Tuple [lower, upper] for hpd/et, or float for ll/ul
+    """
+    if level>=1:
+        raise ValueError('level must be <1, got {0:.2f}'.format(level))
+    if len(np.shape(samples)) != 1:
+        raise ValueError('Support only 1D arrays for samples')
+    if weights is not None and np.shape(samples) != np.shape(weights):
+        raise ValueError('Shape of samples and weights differs')
+
     weights = np.ones(len(samples)) if weights is None else weights
     # Sort and normalize
     order = np.argsort(samples)
@@ -14,7 +45,19 @@ def credibility_interval(samples, weights=None, level=0.68):
     S = np.array([np.min(samples), *samples, np.max(samples)])
     CDF = np.append(np.insert(np.cumsum(weights), 0, 0), 1)
     invCDF = sip.interp1d(CDF, S)
-    # Find smallest interval
-    distance = lambda Y, level=level: invCDF(Y+level)-invCDF(Y)
-    res = sop.minimize_scalar(distance, bounds=(0, 1-level), method="Bounded")
-    return np.array([invCDF(res.x), invCDF(res.x+level)])
+
+    if method == "hpd":
+        # Find smallest interval
+        distance = lambda Y, level=level: invCDF(Y+level)-invCDF(Y)
+        res = sop.minimize_scalar(distance, bounds=(0, 1-level), method="Bounded")
+        return np.array([invCDF(res.x), invCDF(res.x+level)])
+    elif method == "ll":
+        # Get value from which we reach the desired level
+        return invCDF(1-level)
+    elif method == "ul":
+        # Get value to which we reach the desired level
+        return invCDF(level)
+    elif method == "et":
+        return np.array([invCDF((1-level)/2), invCDF((1+level)/2)])
+    else:
+        raise ValueError("Method '{0:}' unknown".format(method))
